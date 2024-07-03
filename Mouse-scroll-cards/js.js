d71108319fc86942868cac11c3ea27c4f3760125
Yurii Mytrofanov <yurii.mytrofanov@dreamdev.info>
2024-07-03 09:39:17 +0300

    Mouse scroll cards

---
+++ Mouse-scroll-cards/js.js
@@ -0,0 +1,26 @@
+// Mouse scroll cards left/right for PC
+function mouseScrollBlock(containerWithCards) {
+    let isDragging = false;
+    let startX, scrollLeft;
+
+    containerWithCards.addEventListener("mousedown", function (e) {
+        isDragging = true;
+        startX = e.pageX - containerWithCards.offsetLeft;
+        scrollLeft = containerWithCards.scrollLeft;
+    });
+
+    containerWithCards.addEventListener("mouseleave", function () {
+        isDragging = false;
+    });
+
+    containerWithCards.addEventListener("mouseup", function () {
+        isDragging = false;
+    });
+
+    containerWithCards.addEventListener("mousemove", function (e) {
+        if (!isDragging) return;
+        let x = e.pageX - containerWithCards.offsetLeft;
+        let walk = (x - startX) * 2;
+        containerWithCards.scrollLeft = scrollLeft - walk;
+    });
+}

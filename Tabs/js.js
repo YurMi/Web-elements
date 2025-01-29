93582574e8c24d3de27af9090823a70474a40ea5
y.mytrofanov <yurii.mytrofanov@dreamdev.info>
2025-01-29 16:25:13 +0100

    update Tabs

--- Tabs/js.js
+++ Tabs/js.js
@@ -1,44 +1,44 @@
+//Tabs ======
 (function () {
-  const tabParent = document.querySelectorAll('[data-tabs]');
-  if (!tabParent.length) return;
+    const tabParent = document.querySelectorAll("[data-tabs]");
+    if (!tabParent.length) return;
 
-  tabParent.forEach((parent) => {
-    const buttons = parent.querySelectorAll('[data-tab-link]');
-    const tabContent = parent.querySelectorAll('[data-tab-content]');
+    tabParent.forEach((parent) => {
+        const buttons = parent.querySelectorAll("[tab-link]");
+        const tabContent = parent.querySelectorAll("[tab-content]");
 
-    //page is load, close all tabs content
-    closeTabs(tabContent);
-
-    buttons.forEach((button) => {
-      //page is load, open active Tab
-      if (button.classList.contains('active-tab-button')) {
-        const activeTabContent = parent.querySelector(
-          `[data-tab-content='${button.getAttribute('data-tab-link')}']`
-        );
-        activeTabContent.classList.add('active-tab-content');
-      }
-
-      button.addEventListener('click', function () {
-        const activeTabContent = parent.querySelector(
-          `[data-tab-content='${button.getAttribute('data-tab-link')}']`
-        );
-
-        buttons.forEach((item) => {
-          item.classList.remove('active-tab-button');
-        });
-
+        //page is load, close all tabs content
         closeTabs(tabContent);
+        let firstActiveTabFound = false;
 
-        //open active Tab
-        this.classList.add('active-tab-button');
-        activeTabContent.classList.add('active-tab-content');
-      });
-    });
-  });
+        buttons.forEach((button) => {
+            //page is load, open active Tab
+            if (!firstActiveTabFound && !button.classList.contains("hide_team_member")) {
+                button.classList.add("active-tab-button");
+                const activeTabContent = parent.querySelector(`[tab-content='${button.getAttribute("tab-link")}']`);
+                activeTabContent.classList.add("active-tab-content");
+                firstActiveTabFound = true;
+            }
 
-  function closeTabs(items) {
-    items.forEach((item) => {
-      item.classList.remove('active-tab-content');
+            button.addEventListener("click", function () {
+                const activeTabContent = parent.querySelector(`[tab-content='${button.getAttribute("tab-link")}']`);
+
+                buttons.forEach((item) => {
+                    item.classList.remove("active-tab-button");
+                });
+
+                closeTabs(tabContent);
+
+                //open active Tab
+                this.classList.add("active-tab-button");
+                activeTabContent.classList.add("active-tab-content");
+            });
+        });
     });
-  }
+
+    function closeTabs(items) {
+        items.forEach((item) => {
+            item.classList.remove("active-tab-content");
+        });
+    }
 })();
